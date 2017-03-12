3dcdf8e30f853257bd166b98e76519218082fdb6
Lucas Gimenez <lucas07gimenez@gmail.com>
2017-03-11 21:54:18 -0300

    Uploaded with the script

--- gitScript/git_script.sh
+++ gitScript/git_script.sh
@@ -58,7 +58,7 @@ echo "INGRESE LA 'URL' DEL REPOSITORIO REMOTO:"
 read url
 
 #Nos conectamos (linkeamos) al repositorio remoto ingresado, donde vamos a subir nuestros archivos.
-
+git remote remove origin
 git remote add origin url
 
 #Para actualizar nuestro repositorio local, respecto al remoto, se ejecutan las siguientes líneas para evitar errores a la hora de subir los archivos.
